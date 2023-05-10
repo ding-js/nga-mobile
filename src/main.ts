9b4e114627f283b2f9cfe45fa971025b839b6194
Ding Yuchao <dingyc@greatld.com>
2023-05-10 14:01:44 +0800

    feat: update list style

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,40 @@
-// ==UserScript==
-// @name         nga mobile
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  改善 nga 移动端体验
-// @author       ding-js
-// @match        *://bbs.nga.cn/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// @run-at       document-body
-// ==/UserScript==
+import { forEach } from 'lodash';
 
-console.log('enter');
+const viewport = document.createElement('meta');
+
+function cleanElements() {
+  ['meta[name=viewport]', '#minWidthSpacer'].forEach((selector) => {
+    const el = document.querySelector(selector);
+    if (el && el.parentElement) {
+      el.parentElement.removeChild(el);
+    }
+  });
+
+  forEach(document.querySelectorAll('style'), (s) => {
+    if (/@-ms-viewport/.test(s.textContent || '')) {
+      s.parentElement?.removeChild(s);
+    }
+  });
+
+  viewport.setAttribute('name', 'viewport');
+  viewport.setAttribute('content', 'width=device-width, initial-scale=1');
+
+  document.head.appendChild(viewport);
+}
+
+function extendStyles() {
+  GM_addStyle(/* css */ `
+    .catenew, .cateblock {
+        font-size: 18px;
+        line-height: 20px;
+    }
+    `);
+}
+
+function main() {
+  cleanElements();
+
+  extendStyles();
+}
+
+main();
